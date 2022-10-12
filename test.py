a6662e8990961d2adbad36709e361c840e859df3
Yuxuan Li <zcablii@ucl.ac.uk>
2022-10-12 11:00:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -24,20 +24,20 @@ def alter(file,old_str,new_str):
     os.remove(file)
     os.rename("%s.bak" % file, file)
 dota_paht = '/'.join(args.preprocessed_path.split('/')[:-1])+'test_dota'
-alter("configs/preprocess/fair1m_1_5_preprocess_config_ms_le90_test.py", '../data/testa-3', args.test_path)
-alter("configs/preprocess/fair1m_1_5_preprocess_config_ms_le90_test.py", '../data/testa_dota', dota_paht)
-alter("configs/preprocess/fair1m_1_5_preprocess_config_ms_le90_test.py", '../data/testa_ms', args.preprocessed_path)
-alter("configs/orcnn_van3_for_test_1.py", '../data/testa_ms', args.preprocessed_path)
-alter("configs/orcnn_van3_for_test_2.py", '../data/testa_ms', args.preprocessed_path)
+alter("configs/preprocess/fair1m_1_5_preprocess_config_ms_le90_test.py", 'source_fair_dataset_path', 'source_fair_dataset_path="' + args.test_path +'"#')
+alter("configs/preprocess/fair1m_1_5_preprocess_config_ms_le90_test.py", 'source_dataset_path', 'source_dataset_path = "'+dota_paht+'"#')
+alter("configs/preprocess/fair1m_1_5_preprocess_config_ms_le90_test.py", 'target_dataset_path', 'target_dataset_path = "'+args.preprocessed_path+'"#')
+alter("configs/orcnn_van3_for_test_1.py", 'images_dir', 'images_dir = "' + args.preprocessed_path + '",#')
+alter("configs/orcnn_van3_for_test_2.py", 'images_dir', 'images_dir = "' + args.preprocessed_path + '",#')
 
-if not os.path.exists(args.preprocessed_path):
-    os.system('python tools/preprocess.py --config-file configs/preprocess/fair1m_1_5_preprocess_config_ms_le90_test.py')
-else:
-    if len(glob.glob(os.path.join(args.preprocessed_path,'test_1024_200_0.5-1.0-1.5/images/'))) == 0:
-        os.system('python tools/preprocess.py --config-file configs/preprocess/fair1m_1_5_preprocess_config_ms_le90_test.py')
+# if not os.path.exists(args.preprocessed_path):
+#     os.system('python tools/preprocess.py --config-file configs/preprocess/fair1m_1_5_preprocess_config_ms_le90_test.py')
+# else:
+#     if len(glob.glob(os.path.join(args.preprocessed_path,'test_1024_200_0.5-1.0-1.5/images/'))) == 0:
+#         os.system('python tools/preprocess.py --config-file configs/preprocess/fair1m_1_5_preprocess_config_ms_le90_test.py')
 
-if not os.path.exists('submit_zips/orcnn_van3_for_test_1_epoch0.csv'):
-    os.system('python tools/run_net.py --config-file configs/orcnn_van3_for_test_1.py --task test')
-if not os.path.exists('submit_zips/orcnn_van3_for_test_2_epoch0.csv'):
-    os.system('python tools/run_net.py --config-file configs/orcnn_van3_for_test_2.py --task test')
-os.system('python merge.py')
+# if not os.path.exists('submit_zips/orcnn_van3_for_test_1_epoch0.csv'):
+#     os.system('python tools/run_net.py --config-file configs/orcnn_van3_for_test_1.py --task test')
+# if not os.path.exists('submit_zips/orcnn_van3_for_test_2_epoch0.csv'):
+#     os.system('python tools/run_net.py --config-file configs/orcnn_van3_for_test_2.py --task test')
+# os.system('python merge.py')
